b864c622f00c38d2ef69395e55851950584c03a5
Булат Х <hatmullinbulat24@gmail.com>
2025-05-28 18:04:43 +0500

    INIT PROJECT

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="app-layout">
+        <header class="mb-4">
+            <div class="py-4">
+                <nav class="flex items-center gap-4">
+                    <NuxtLink to="/">Главная</NuxtLink>
+                    <NuxtLink to="/about">О нас</NuxtLink>
+                </nav>
+            </div>
+        </header>
+
+        <slot />
+
+        <footer class="mt-auto">
+            <div class="py-4">
+                &copy; 2024 Булат и Виталий
+            </div>
+        </footer>
+    </div>
+</template>
+
+<style scoped>
+.app-layout {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <NuxtLayout>
+        <div>
+        <h1 class="text-2xl">
+            Attestation
+        </h1>
+    </div>
+    </NuxtLayout>
+</template>
